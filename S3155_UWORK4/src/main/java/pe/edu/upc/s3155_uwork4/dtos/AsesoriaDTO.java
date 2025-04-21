fc0e14f07b6ef439cfb74b2fa5bcf1024e526d72
ArianaDelCarpio <U202211917@upc.edu.pe>
2025-04-21 02:06:07 -0500

    Creacion tabla Asesoria

--- S3155_UWORK4/src/main/java/pe/edu/upc/s3155_uwork4/dtos/AsesoriaDTO.java
+++ S3155_UWORK4/src/main/java/pe/edu/upc/s3155_uwork4/dtos/AsesoriaDTO.java
@@ -1,4 +1,53 @@
 package pe.edu.upc.s3155_uwork4.dtos;
 
+import pe.edu.upc.s3155_uwork4.entities.Usuario;
+
+import java.time.LocalDate;
+
 public class AsesoriaDTO {
+    private int idAsesoria;
+    private String nombreAsesoria;
+    private LocalDate fechaAsesoria;
+    private Usuario usuarioInferior;
+    private Usuario usuarioSuperior;
+
+    public int getIdAsesoria() {
+        return idAsesoria;
+    }
+
+    public void setIdAsesoria(int idAsesoria) {
+        this.idAsesoria = idAsesoria;
+    }
+
+    public String getNombreAsesoria() {
+        return nombreAsesoria;
+    }
+
+    public void setNombreAsesoria(String nombreAsesoria) {
+        this.nombreAsesoria = nombreAsesoria;
+    }
+
+    public LocalDate getFechaAsesoria() {
+        return fechaAsesoria;
+    }
+
+    public void setFechaAsesoria(LocalDate fechaAsesoria) {
+        this.fechaAsesoria = fechaAsesoria;
+    }
+
+    public Usuario getUsuarioInferior() {
+        return usuarioInferior;
+    }
+
+    public void setUsuarioInferior(Usuario usuarioInferior) {
+        this.usuarioInferior = usuarioInferior;
+    }
+
+    public Usuario getUsuarioSuperior() {
+        return usuarioSuperior;
+    }
+
+    public void setUsuarioSuperior(Usuario usuarioSuperior) {
+        this.usuarioSuperior = usuarioSuperior;
+    }
 }
--- S3155_UWORK4/src/main/java/pe/edu/upc/s3155_uwork4/servicesinterfaces/IAsesoriaService.java
+++ S3155_UWORK4/src/main/java/pe/edu/upc/s3155_uwork4/servicesinterfaces/IAsesoriaService.java
@@ -1,4 +1,15 @@
 package pe.edu.upc.s3155_uwork4.servicesinterfaces;
 
+import pe.edu.upc.s3155_uwork4.entities.Asesoria;
+import pe.edu.upc.s3155_uwork4.entities.Rol;
+
+import java.util.List;
+
 public interface IAsesoriaService {
+    public List<Asesoria> listar();
+    public void Registrar(Asesoria r);
+    public Asesoria listarporid(int id);
+    public void Modificar(Asesoria r);
+    public void Eliminar(int id);
+
 }
